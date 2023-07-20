200aa5a050373a1293b67b689dac469f8d5832d9
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-07-20 13:47:14 +0000

    Chore: Enhance test and task functionality
    - Update dev and test monitor script to better handle file
      modifications. Now the rerun only happens after a `closed` event.
    - Update vscode tasks to not steal focus.

--- scripts/test-monitor.sh
+++ scripts/test-monitor.sh
@@ -4,8 +4,11 @@ echo "Starting watchmedo for pytest…"
 watchmedo shell-command \
   --patterns "*.py" \
   --recursive \
-  --command "\
-      pytest src \
-    " \
-  .
-  
+  --wait \
+  --verbose \
+  --command '
+      if [ "${watch_event_type}" = "closed" ]; then
+        pytest src/network_delay 
+      fi
+    '\
+  src
